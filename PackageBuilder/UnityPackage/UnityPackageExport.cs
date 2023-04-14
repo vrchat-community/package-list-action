18af6a408f9a547fef14d3c4f20a220a270f93a4
Momo The Monster <momodmonster@gmail.com>
2023-04-14 13:55:31 -0700

    Uses Glob instead of Matcher

--- PackageBuilder/UnityPackage/UnityPackageExport.cs
+++ PackageBuilder/UnityPackage/UnityPackageExport.cs
@@ -2,7 +2,8 @@
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
-using Microsoft.Extensions.FileSystemGlobbing;
+using GlobExpressions;
+// using Microsoft.Extensions.FileSystemGlobbing;
 using Nuke.Common;
 using Nuke.Common.IO;
 using Serilog;
@@ -72,23 +73,53 @@ partial class Build
             using DependencyAnalyser analyser = !skipDep ? await DependencyAnalyser.CreateAsync(unityPackageExportSource / assetRoot, excludePattern) : null;
             using Packer packer = new Packer(unityPackageExportSource, unityPackageExportOutput);
 
-            // Match all the assets we need
-            Matcher assetMatcher = new Matcher();
-            assetMatcher.AddIncludePatterns(assetPattern);
-            foreach (string s in assetPattern)
-            {
-                Log.Information($"Added includePattern {s}");
-            }
-            // assetMatcher.AddExcludePatterns(excludePattern);
-            // foreach (string s in excludePattern)
-            // {
-            //     Log.Information($"Added excludePattern {s}");
-            // }
-            assetMatcher.AddExclude(unityPackageExportOutput);
-            Log.Information($"Added exclude {unityPackageExportOutput}");
+            // Use Nuke's globbing to match the assets
+            
+            // Get all files in the directory and its subdirectories
+            var rootPath = unityPackageExportSource;
+            var allFiles = Directory.GetFiles(rootPath, "*.*", SearchOption.AllDirectories);
 
-            var matchedAssets = assetMatcher.GetResultsInFullPath(unityPackageExportSource);
+            List<string> matchedAssets = new List<string>();
+            // Iterate through the files and apply the include and exclude patterns
+            Log.Information("Matched files:");
+            foreach (var filePath in allFiles)
+            {
+                // Get the relative path to the root directory
+                string relativePath = Path.GetRelativePath(rootPath, filePath);
+
+                // Check if the file matches any of the include patterns and does not match any of the exclude patterns
+                bool isIncluded = AnyPatternMatches(relativePath, assetPattern);
+                bool isExcluded = AnyPatternMatches(relativePath, excludePattern);
+
+                if (isIncluded && !isExcluded)
+                {
+                    Log.Information($"Matched file: {relativePath}");
+                    matchedAssets.Add(relativePath);
+                }
+            }
+            
+            // Match all the assets we need
+            // Matcher assetMatcher = new Matcher();
+            // assetMatcher.AddIncludePatterns(assetPattern);
+            // foreach (string s in assetPattern)
+            // {
+            //     Log.Information($"Added includePattern {s}");
+            // }
+            // // assetMatcher.AddExcludePatterns(excludePattern);
+            // // foreach (string s in excludePattern)
+            // // {
+            // //     Log.Information($"Added excludePattern {s}");
+            // // }
+            // assetMatcher.AddExclude(unityPackageExportOutput);
+            // Log.Information($"Added exclude {unityPackageExportOutput}");
+            //
+            // var matchedAssets = assetMatcher.GetResultsInFullPath(unityPackageExportSource);
+            //
+            Assert.True(matchedAssets.Count() > 0, "No assets matched the pattern. Please check your pattern and try again.");
+            
             Log.Information($"Found {matchedAssets.Count()} matched assets");
+            
+            
             await packer.AddAssetsAsync(matchedAssets);
 
             if (!skipDep)
@@ -101,4 +132,17 @@ partial class Build
             Log.Information($"Finished Packing in {timer.ElapsedMilliseconds}ms to {packer.OutputPath}");
             await packer.FlushAsync();
         });
+    
+    // Helper method to check if any pattern in the given array matches the provided path
+    static bool AnyPatternMatches(string path, string[] patterns)
+    {
+        foreach (var pattern in patterns)
+        {
+            if (Glob.IsMatch(path, pattern))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
 }
